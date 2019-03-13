350a830a7e52d9d30f4a66eff6e3b421e846a080
Yassin Moghazy <48529579+ymoghazy@users.noreply.github.com>
2019-03-13 14:43:03 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,3 +1,12 @@
 public class SumSquare{
-  
+  public static void main(String [] args){
+    int x = 3;
+    int sum = 0;
+    while (x <= 33){
+      sum += Math.pow(x , 2);
+      System.out.println(sum);
+      x += 3;
+  }
+
+
 }
