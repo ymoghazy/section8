16c799c6dfc3892167dfe4cf1160c7815f6d9d09
AnitaSafo <48529746+AnitaSafo@users.noreply.github.com>
2019-03-13 15:08:39 -0400

    Update LetterWeight.java

--- LetterWeight.java
+++ LetterWeight.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class LetterWeight{
   public static void main(String [] args){
+    Scanner input = new Scanner(System.in);
     
+   double weight;
+     while (true){
+       System.out.println("Enter a weight in ounces for letters to mail out.");
+       System.out.print("When yoou are done entering, please enter -1 to finish: ");
+       weight = input.nextDouble();
+       if( weight > 0 && weight <= 1) {
+        System.out.println("You can use forever stamp");
+       }
+       else if(weight > 1){
+         System.out.println("You need additional postage than a forever stamp");
+       }
+       else {
+         System.out.print("Goodbye!!");
+         System.exit(0);
+       }
+     }
   }
 }
