1b4ebec85eaf5da9154662314e4e5771269317a0
Yassin Moghazy <48529579+ymoghazy@users.noreply.github.com>
2019-03-13 15:21:36 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -1,12 +1,13 @@
-public class SumSquare{
-  public static void main(String [] args){
-    int x = 3;
-    int sum = 0;
-    while (x <= 33){
-      sum += Math.pow(x , 2);
-      System.out.println(sum);
-      x += 3;
-  }
-
-
+public class SumSquare {
+	public static void main(String [] args){
+		int x = 3;
+		int sum = 0;
+		while (x <= 33){
+			sum += Math.pow(x , 2);
+		    System.out.printf("Cumulation results: %-8d :         %-8d *         %-8d%n", sum , x , x );
+		    x += 3;
+		}
+		System.out.print("The SUM of Square of muiples of 3 is " + sum);
+	}	  
 }
+
